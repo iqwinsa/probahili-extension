f41892d581f7867378e85803d5da3df05deef339
iqwinsa <podkovtcev@yandex.ru>
2023-01-09 13:53:46 +0300

    Обновлена логика работы с селектором Склад в МойСклад

--- js/moysklad.js
+++ js/moysklad.js
@@ -4,23 +4,37 @@ domain = window.location.host.split(".")[1]
 
 // console.log(apiKey)
 if (domain === "moysklad") {
-	let ids = [ "SystemFields.warehouse", 
-				"SystemFields.targetAgent",
-				"SystemFields.sourceAgent",
-				"SystemFields.planningShipmentDate",
-				// "c45e96366-06a5-11e8-7a69-97110010ff2a",
-				// "c79e11084-2ad2-11e9-9ff4-34e800065ab7",
-				// "c02da1550-195c-11eb-0a80-06c50001633a",
-				// "ca5c52b0a-0fff-11e8-9ff4-31500003dcf5",
-				// "cbe8d2bcf-e41a-11e8-9ff4-315000140059",
-				// "c3f09d382-2b8c-11e9-9ff4-3150000f37f6",
-				// "cf61aa183-267a-11eb-0a80-030b00163c65",
-				// "c6c03b0a4-286b-11e8-9ff4-34e8000b5ad9",
-				// "c1b229da7-20b8-11ea-0a80-064c000668d4",
-				// "ca9a40cc6-8af6-11ea-0a80-0148000a58ae"
+	let ids = [
+			// "SystemFields.warehouse", 
+			"SystemFields.targetAgent",
+			"SystemFields.sourceAgent",
+			"SystemFields.planningShipmentDate"
 			]
+	
+	let flagHouse = false;
+	let currentWareHouseName = "";
 
 	setInterval(function() {
+		
+		let currentWareHouse = $('div[data-test-id="SystemFields.warehouse"] input');	
+		if (currentWareHouse.length > 0) {			
+			if (!flagHouse) {
+				currentWareHouseName = currentWareHouse.val();
+				console.log(currentWareHouseName);
+				flagHouse = true;
+			}
+
+			let actualWarehouseName = currentWareHouse.val();
+
+			if (flagHouse) {
+				if (currentWareHouseName != actualWarehouseName) {
+					$('div[data-test-id="editor-toolbar-save-button"]').hide();
+				} else {
+					$('div[data-test-id="editor-toolbar-save-button"]').show();
+				}
+			}
+		}
+
 		num = null
 		//	console.log(document.addEventListener("DOMContentLoaded", ready))
 		hash = window.location.hash.split("=")
@@ -63,21 +77,6 @@ if (domain === "moysklad") {
 				});
 			}
 		}
-		// let saveBtn = document.getElementsByClassName("b-popup-button b-popup-button-enabled b-popup-button-green tutorial-save-button")[0]
-		// saveBtn.style.display = "none";
-		// console.log("text: " + host)
 	}, 400);
 
 }
-
-// setInterval(function() {
-// if (!apiKey) {
-// // console.log(apiKey)
-// fetch('https://xn--80abwmlfh7b4c.xn--p1ai/integrations/extension_stocks/ms.php?request=true&site=' + host)
-// .then(response => response.text())
-// .then(result => { apiKey = result, localStorage.setItem('apiKey', apiKey);})
-// console.log('Probahili Extension loaded with API key')
-// } else {
-//   return false
-// }
-// }, 1000);
